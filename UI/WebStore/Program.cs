18c3ee0746262ab80730e1d07fe6707051d09df6
Илья Тимофеев <Mcdos14@ya.ru>
2021-10-22 01:36:52 +0500

    добавление и конфигурация Serilog

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 using WebStore.Services.Data;
 
 namespace WebStore
@@ -25,6 +28,16 @@ namespace WebStore
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+
+                })
+            .UseSerilog((host, log) => log.ReadFrom.Configuration(host.Configuration)
+                   .MinimumLevel.Debug()
+                   .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                   .Enrich.FromLogContext()
+                   .WriteTo.Console(
+                        outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level:u3}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+                   .WriteTo.RollingFile($@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log")
+                   .WriteTo.File(new JsonFormatter(",", true), $@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log.json")
+                );
     }
 }
