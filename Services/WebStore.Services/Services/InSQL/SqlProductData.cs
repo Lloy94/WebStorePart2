f55e00c5d9e9b62badd016cf7bfa1c0bde332834
Илья Тимофеев <Mcdos14@ya.ru>
2021-11-09 11:31:12 +0500

    Правка скрипта

--- Services/WebStore.Services/Services/InSQL/SqlProductData.cs
+++ Services/WebStore.Services/Services/InSQL/SqlProductData.cs
@@ -27,18 +27,15 @@ namespace WebStore.Services.Services.InSQL
             IQueryable<Product> query = _db.Products
                .Include(p => p.Brand)
                .Include(p => p.Section);
-
             if (Filter?.Ids?.Length > 0)
                 query = query.Where(product => Filter.Ids.Contains(product.Id));
             else
             {
                 if (Filter?.SectionId is { } section_id)
                     query = query.Where(p => p.SectionId == section_id);
-
                 if (Filter?.BrandId is { } brand_id)
                     query = query.Where(p => p.BrandId == brand_id);
             }
-
             var total_count = query.Count();
 
             if (Filter is { PageSize: > 0 and var page_size, Page: > 0 and var page_number })
