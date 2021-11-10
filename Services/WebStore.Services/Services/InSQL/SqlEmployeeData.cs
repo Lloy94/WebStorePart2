f2b6b6cec848f3509e0280b1c6a407143d05a388
Илья Тимофеев <Mcdos14@ya.ru>
2021-11-10 16:09:14 +0500

    Дописание сервиса хранения сотрудников/добавление его в класс инициализации базы данных

--- Services/WebStore.Services/Services/InSQL/SqlEmployeeData.cs
+++ Services/WebStore.Services/Services/InSQL/SqlEmployeeData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebStore.DAL.Context;
 using WebStore.Domain.Models;
 using WebStore.Interfaces.Services;
 
@@ -10,29 +11,66 @@ namespace WebStore.Services.Services.InSQL
 {
     public class SqlEmployeeData : IEmployeeData
     {
+        private readonly WebStoreDB _db;
+        private int _CurrentMaxId;
+
+        public SqlEmployeeData(WebStoreDB db) 
+        { 
+            _db = db;
+            _CurrentMaxId = _db.Employees.Last().Id;
+        }
+
         public int Add(Employee employee)
         {
-            throw new NotImplementedException();
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+            if (_db.Employees.Contains(employee)) return employee.Id;
+
+            employee.Id = ++_CurrentMaxId;
+            _db.Employees.Add(employee);
+            _db.SaveChanges();
+
+            return employee.Id;
+
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            var db_employee = GetById(id);
+            if (db_employee is null) return false;
+
+            _db.Employees.Remove(db_employee);
+
+            return true;
         }
 
         public IEnumerable<Employee> GetAll()
         {
-            throw new NotImplementedException();
+           return _db.Employees;
         }
 
-        public Employee GetById(int id)
+        public  Employee GetById(int id)
         {
-            throw new NotImplementedException();
+           return _db.Employees.Find(id);
         }
 
         public void Update(Employee employee)
         {
-            throw new NotImplementedException();
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+            if (_db.Employees.Contains(employee)) return;
+
+            var db_employee = GetById(employee.Id);
+
+            if (db_employee is null) return;
+
+            db_employee.LastName = employee.LastName;
+            db_employee.Name = employee.Name;
+            db_employee.Patronymic = employee.Patronymic;
+            db_employee.Age = employee.Age;
+            db_employee.Info = employee.Info;
+
+            _db.SaveChanges();
         }
     }
 }
