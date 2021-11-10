4d5096f86ea9ee736108692931f673ad31a7810b
Илья Тимофеев <Mcdos14@ya.ru>
2021-11-10 15:30:02 +0500

    Заготовка сервиса хранилища сотрудников в базе данных

---
+++ Services/WebStore.Services/Services/InSQL/SqlEmployeeData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Models;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Services.Services.InSQL
+{
+    public class SqlEmployeeData : IEmployeeData
+    {
+        public int Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
